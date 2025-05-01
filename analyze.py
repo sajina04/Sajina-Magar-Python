20bdf6933e57b66b4ff8378712374529e38fba4a
Sajina Magar <magarsajina460@gmail.com>
2025-05-01 16:54:55 +0545

    imported the necessary tools

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+
+
+
